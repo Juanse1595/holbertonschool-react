16b7d148baac29d15e48257d166f5fd8a61f6bee
Juanse1595 <jspbescos15@gmail.com>
2022-06-09 22:02:28 -0500

    ADD task3

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+
+class BodySection extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className='bodySection'>
+                <h2>{this.props.title}</h2>
+                {this.props.childern}
+            </div>
+        )
+    }
+}

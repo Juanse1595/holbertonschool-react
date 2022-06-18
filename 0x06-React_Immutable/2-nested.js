9e0b854d8c73a5841efd564f13df506d14fb7636
Juanse1595 <jspbescos15@gmail.com>
2022-06-18 18:35:46 -0500

    ADD 3rd task file and fix eslint 2nd

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
 import Immutable from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    return Immutable.Map(object).getIn(array)
-}
+  return Immutable.Map(object).getIn(array);
+};

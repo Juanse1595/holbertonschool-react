01c03aada8a3fb1611ef035334f4cd5218f8b0fc
Juanse1595 <jspbescos15@gmail.com>
2022-06-20 17:52:33 -0500

    ADD 8th

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,13 @@
+import Immutable from 'immutable';
+
+export default function printBestStudents(obj) {
+  const bestStudents = Immutable.Seq(obj).filter((elm) => elm.score > 70);
+  const bestStudentsObj = bestStudents.toJS();
+  Object.keys(bestStudentsObj).forEach((key) => {
+    bestStudentsObj[key].firstName = bestStudentsObj[key].firstName.charAt(0).toUpperCase()
+    + bestStudentsObj[key].firstName.slice(1);
+    bestStudentsObj[key].lastName = bestStudentsObj[key].lastName.charAt(0).toUpperCase()
+    + bestStudentsObj[key].lastName.slice(1);
+  });
+  console.log(bestStudentsObj);
+}
